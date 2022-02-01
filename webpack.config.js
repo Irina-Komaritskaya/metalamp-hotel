3a61e14da2595bdb0d22a64afc417db0390b7198
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2022-02-01 21:42:30 +0700

    color and type

--- webpack.config.js
+++ webpack.config.js
@@ -5,52 +5,55 @@ const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
-  mode: "development",
-  devServer: {
-    watchFiles: ["src/**/*"],
-    static: { directory: path.join(__dirname, "./dist/html") },
-    compress: true,
-    open: true,
-    port: 4000,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [ MiniCssExtractPlugin.loader,  "css-loader", "sass-loader"], // isDev ? miniCss : "style-loader", 
-      },
-      { test: /\.css$/, use: ["css-loader", MiniCssExtractPlugin.loader] },
-      { test: /\.(js)$/, use: "babel-loader" },
-      {
-        test: /\.pug$/,
-        loader: "pug-loader",
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-            options: { name: "./[name].[ext]" },
-          },
-        ],
-      },
-      { test: /\.svg$/, use: "file-loader" },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/pages/index.pug",
-      filename: "./html/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "./style.css",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPugPlugin(),
-  ],
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index_bundle.js",
-  },
+	entry: path.resolve(__dirname, "./src/index.js"),
+	mode: "development",
+	devServer: {
+		watchFiles: ["src/**/*"],
+		static: { directory: path.join(__dirname, "./dist/html/") },
+		compress: true,
+		open: true,
+		port: 4000,
+		historyApiFallback: {
+			index: "color-and-type.html",
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"], // isDev ? miniCss : "style-loader",
+			},
+			{ test: /\.css$/, use: ["css-loader", MiniCssExtractPlugin.loader] },
+			{ test: /\.(js)$/, use: "babel-loader" },
+			{
+				test: /\.pug$/,
+				loader: "pug-loader",
+			},
+			{
+				test: /\.(png|jpg|gif)$/i,
+				use: [
+					{
+						loader: "file-loader",
+						options: { name: "./[name].[ext]" },
+					},
+				],
+			},
+			{ test: /\.svg$/, use: "file-loader" },
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/pages/kit/color-and-type/color-and-type.pug",
+			filename: "./html/color-and-type.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "./style.css",
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPugPlugin(),
+	],
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index_bundle.js",
+	},
 };
