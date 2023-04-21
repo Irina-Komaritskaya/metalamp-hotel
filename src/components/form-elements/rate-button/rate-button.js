f7e825eee031b10803e02b410dddbc352d6304e5
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2023-04-21 19:59:13 +0200

    rate-button

--- src/components/form-elements/rate-button/rate-button.js
+++ src/components/form-elements/rate-button/rate-button.js
@@ -1,11 +1,24 @@
-$(".rate-button").on("mouseover", (e) => {
-    const star = e.currentTarget;
-    const state = $(star).closest(".rate-buttons").attr("data-state");
-    if (state === "undisabled") {
-        const icon = $(star).find(".icon-button");
-        const text = icon.text() === "star_border" ? "star" : "star_border";
-        const nottext = icon.text() != "star_border" ? "star" : "star_border";
-        $(star).nextAll(".rate-button").find(".icon-button").text(text);
-        // $(star).prevAll(".rate-button").find(".icon-button").text(nottext);
-    }
+const toggleStar = (el) => {
+    $(el).prevAll(".rate-button").find(".icon-button").text("star");
+    $(el).nextAll(".rate-button").find(".icon-button").text("star_border");
+};
+
+$(".rate-buttons[data-state='undisabled']").on("mouseout", (e) => {
+    const parent = e.currentTarget;
+    const rate = $(parent).attr("data-rate");
+    const children = $(parent).children()[rate - 1];
+
+    toggleStar(children);
+    $(children).find(".icon-button").text("star");
 });
+
+$(".rate-buttons[data-state='undisabled'] > .rate-button").on(
+    "mouseover",
+    (e) => {
+        const star = e.currentTarget;
+        const icon = $(star).find(".icon-button");
+
+        toggleStar(star);
+        icon.text("star");
+    }
+);
--- src/index.js
+++ src/index.js
@@ -4,9 +4,3 @@ import "nouislider/dist/nouislider.css";
 import "./scss/main.scss";
 import "./pages/kit/form-elements/form-elements.pug";
 import "./components/index.js";
-let template = require("./pages/kit/form-elements/form-elements.pug");
-let locals = {
-    users: 5,
-};
-
-document.querySelector("main").innerHTML = template(locals);
