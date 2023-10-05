b9b8b6e42261f297789988288fafbdbc2e8b3f3e
Irina-Komaritskaya <amsterdam.ira@yandex.ru>
2023-10-05 12:35:22 +0200

    pagination hide and show arrow, variable for title

--- src/components/form-elements/pagination/pagination.js
+++ src/components/form-elements/pagination/pagination.js
@@ -2,56 +2,41 @@ import "../../../../node_modules/paginationjs/dist/pagination";
 $(function () {
     const pageSize = 12;
     const dataContainer = "";
-    const dataSize = 196;
-    const countPage = dataSize / pageSize;
-    $(".pagination__item").first().addClass("pagination__item-hide");
-});
+    const dataSize = 180;
+    const countPage = Math.ceil(dataSize / pageSize);
+    $(".pagination__item").first().hide();
 
-$(".pagination__item").on("click", (e) => {
-    const item = e.currentTarget;
-    const parent = $(item).closest(".pagination__items");
-    const number = $(item).text();
-    parent
-        .find(".pagination__item-active")
-        .removeClass("pagination__item-active");
-
-    $(item).addClass("pagination__item-active");
-
-    if (number === "1") {
+    $(".pagination__item").on("click", (e) => {
+        const item = e.currentTarget;
+        const parent = $(item).closest(".pagination__items");
+        const firstItem = parent.find(".pagination__item").first();
+        const lastItem = parent.find(".pagination__item").last();
+        const number = $(item).text();
         parent
-            .find(".pagination__item")
-            .first()
-            .addClass("pagination__item-hide");
-    }
-    if (number != "1") {
-        // или если последняя
-        parent
-            .find(".pagination__item-hide")
-            .removeClass("pagination__item-hide");
-    }
-});
+            .find(".pagination__item-active")
+            .removeClass("pagination__item-active");
 
-// свой пагинат
-// $("#pagination").pagination({
-//     dataSource: function (done) {
-//         var result = [];
-//         for (var i = 1; i < 196; i++) {
-//             result.push(i);
-//         }
-//         done(result);
-//     },
-//     pageSize: 12,
-//     showNavigator: true,
-//     autoHideNext: true,
-//     autoHidePrevious: true,
-//     classPrefix: "1",
-//     className: "pagination",
-//     ulClassName: "pagination__pages",
-//     pageClassName: "pagination__page",
-//     activeClassName: "pagination__button-active",
-//     callback: function (data, pagination) {
-//         // template method of yourself
-//         var html = template(data);
-//         dataContainer.html(html);
-//     },
-// });
+        $(item).addClass("pagination__item-active");
+
+        if (number === "1") {
+            parent
+                .find(".pagination__item")
+                .first()
+                .hide({ direction: "left" }, 500);
+        }
+        if (number != "1") {
+            console.log(countPage);
+            firstItem.show({ direction: "left" }, 500);
+        }
+
+        if (number === countPage.toString()) {
+            console.log(lastItem);
+            lastItem.hide({ direction: "right" }, 500);
+        }
+
+        if (number != countPage.toString()) {
+            console.log(lastItem);
+            lastItem.show({ direction: "right" }, 500);
+        }
+    });
+});
